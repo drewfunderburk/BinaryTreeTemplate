0e3564dbf0404921ac21486462681891eba2d649
Drew Funderburk <thatfunderburkguy@gmail.com>
2021-02-24 09:15:04 -0600

    Added a few comments

--- CDDS_BinaryTree/TreeNode.cpp
+++ CDDS_BinaryTree/TreeNode.cpp
@@ -9,15 +9,20 @@ TreeNode::TreeNode(int value)
 
 void TreeNode::draw(int x, int y, bool selected)
 {
+	// Create char array  to store value
 	static char buffer[10];
+	// Use sprintf to write m_value to buffer
 	sprintf(buffer, "%d", m_value);
 
+	// Draw the node to the screen
 	DrawCircle(x, y, 30, YELLOW);
 
+	// Change color if selected
 	if (selected)
 		DrawCircle(x, y, 28, GREEN);
 	else
 		DrawCircle(x, y, 28, BLACK);
 
+	// Draw value over the node
 	DrawText(buffer, x - 12, y - 12, 12, WHITE);
 }
